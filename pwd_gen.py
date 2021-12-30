96c32c4eba806e868f8105e9f2ff6bcc939c8ce9
benni347 <cdrc+gitkraken@skwar.me>
2021-12-30 11:39:36 +0100

    Fix all pylint and flake8 warnings

--- pwd_gen.py
+++ pwd_gen.py
@@ -13,11 +13,16 @@ class PasswordGenerator:
         print("Welcome to the password generator.")
 
         self._password_length = 0
-        self._allowed_alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        self._allowed_digits = "0123456789"
-        self._allowed_symbols = "!@#$%^&*()_+-=[]{}<>?|\\:;'\".,/"
+        self._password = ""
+        self._allowed_classes = {
+            "alpha": "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            "digits": "0123456789",
+            "symbols": "!@#$%^&*()_+-=[]{}<>?|\\:;'\".,/"
+        }
+
         self.ask_password_length()
-        self._alpha, self._numbers, self._symbols = self._ask_allowed_characters()
+        (self._alpha, self._numbers,
+         self._symbols) = self._ask_allowed_characters()
 
     def _get_password_length(self):
         return self._password_length
@@ -25,12 +30,13 @@ class PasswordGenerator:
     def _set_password_length(self, length):
         self._password_length = length
 
-    # This is a helper function that will be used to generate a random password.
-    # ask the user for the password length
     def ask_password_length(self) -> None:
+        """
+This is a helper function that will be used to generate a random password.
+ask the user for the password length
+        """
         # ask the user for the password length
         while True:
-            # ask the user for the password length
             password_length = input(
                 "Please enter the length of the password: ")
             # check if the user entered a good value
@@ -40,36 +46,34 @@ class PasswordGenerator:
                 break
 
     def _verify_password_length(self, password_length):
-        if password_length.isdigit() == False:
+        return_value = False
+        if not password_length.isdigit():
             # if the user entered a non-numeric value, then ask again
             print("You must enter a number.")
-            return False
         elif int(password_length) < 8:
             # if the user entered a number that is less than 8, then ask again
             print("The password must be at least 8 characters long.")
-            return False
+        else:
+            # the user entered a good value
+            return_value = True
 
-        # the user entered a good value
-        return True
+        return return_value
 
     def generate_password(self) -> str:
         """This function will generate a random password."""
-        # generate a random password
-        # create a string of random characters
-        # Allowed characters are:
-        # a-z, A-Z, 0-9, !@#$%^&* and space ( ) { } [ ] < > ? _ + - = ~ ` | \ : ; ' " . , /
         allowed_characters = ''
         if self._alpha:
-            allowed_characters += self._allowed_alpha
+            allowed_characters += self._allowed_classes["alpha"]
         if self._numbers:
-            allowed_characters += self._allowed_digits
+            allowed_characters += self._allowed_classes["digits"]
         if self._symbols:
-            allowed_characters += self._allowed_symbols
+            allowed_characters += self._allowed_classes["symbols"]
 
         password = ''.join(random.choice(allowed_characters)
                            for _ in range(int(self._get_password_length())))
         # save the password
         self._set_password(password)
+        return password
 
     def _get_password(self):
         return self._password
